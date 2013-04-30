fa5d2280d32085742010344efda20ec1ccea93f2
Tim Caswell <tim@creationix.com>
2013-04-30 14:39:46 -0500

    Add shell for demo packaged app

---
+++ demo/app.js
@@ -0,0 +1,7 @@
+console.log("Welcome to the js-git demo.  There are two global objects you can use to manupulate the sandbox.  They are `fs` and `git`.  Use autocomplete to explore their capabilities");
+window.fs = {};
+window.git = {};
+window.env = {};
+window.db = {};
+
+
