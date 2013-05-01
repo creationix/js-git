e18beddaf853a92ff037021ce458623cc2230af9
Tim Caswell <tim@creationix.com>
2013-05-01 12:24:59 -0500

    Improve state machine using array of functions

--- demo/module.js
+++ demo/module.js
@@ -53,7 +53,6 @@ function realGet(path, callback, errback) {
     errback(this);
   };
   request.open("GET", path + "?" + (Math.random() * 0x100000000).toString(36), true);
-  request.setRequestHeader("Pragma", "no-cache");
   request.send();
 }
 ////////////////////////////////////////////////////////////////////////////////
@@ -250,83 +249,93 @@ function realProcess(path, contents, callback, errback) {
 // Extract all require calls using a proper state-machine parser.
 function mine(js) {
   var names = [];
-  var state = "START";
+  var state = 0;
   var ident;
   var quote;
   var name;
-  for (var i = 0, l = js.length; i < l; i++) {
-    var char = js[i];
-    switch (state) {
-      case "START":
-        if (char === "/") state = "SLASH";
-        else if (char === "'" || char === '"') {
-          quote = char;
-          state = "STRING";
-        }
-        else if (char === "r") {
-          ident = char;
-          state = "IDENT";
-        }
-        break;
-      case "IDENT":
-        if (char === "require"[ident.length]) {
-          ident += char;
-        }
-        else if (char === "(" && ident === "require") {
-          ident = undefined;
-          state = "CALL";
-        }
-        else {
-          state = "START";
-        }
-        break;
-      case "CALL":
-        if (char === "'" || char === '"') {
-          quote = char;
-          name = "";
-          state = "NAME";
-        }
-        else {
-          state = "START";
-        }
-        break;
-      case "NAME":
-        if (char === quote) {
-          names.push(name);
-          name = undefined;
-          state = "START";
-        }
-        else {
-          name += char;
-        }
-        break;
-      case "STRING":
-        if (char === "\\") {
-          state = "ESCAPE";
-        }
-        else if (char === quote) {
-          state = "START";
-        }
-        break;
-      case "ESCAPE":
-        state = "STRING";
-        break;
-      case "SLASH":
-        if (char === "/") state = "LINE_COMMENT";
-        else if (char === "*") state = "MULTILINE_COMMENT";
-        else state = "START";
-        break;
-      case "LINE_COMMENT":
-        if (char === "\r" || char === "\n") state = "START";
-        break;
-      case "MULTILINE_COMMENT":
-        if (char === "*") state = "MULTILINE_ENDING";
-        break;
-      case "MULTILINE_ENDING":
-        if (char === "/") state = "START";
-        else if (char !== "*") state = "MULTILINE_COMMENT";
-        break;
+  var states = [
+    // 0 - START
+    function (char) {
+      if (char === "/") state = 6;
+      else if (char === "'" || char === '"') {
+        quote = char;
+        state = 4;
+      }
+      else if (char === "r") {
+        ident = char;
+        state = 1;
+      }
+    },
+    // 1 - IDENT
+    function (char) {
+      if (char === "require"[ident.length]) {
+        ident += char;
+      }
+      else if (char === "(" && ident === "require") {
+        ident = undefined;
+        state = 2;
+      }
+      else {
+        state = 0;
+      }
+    },
+    // 2 - CALL
+    function (char) {
+      if (char === "'" || char === '"') {
+        quote = char;
+        name = "";
+        state = 3;
+      }
+      else {
+        state = 0;
+      }
+    },
+    // 3 - NAME
+    function (char) {
+      if (char === quote) {
+        names.push(name);
+        name = undefined;
+        state = 0;
+      }
+      else {
+        name += char;
+      }
+    },
+    // 4 - STRING
+    function (char) {
+      if (char === "\\") {
+        state = 5;
+      }
+      else if (char === quote) {
+        state = 0;
+      }
+    },
+    // 5 - ESCAPE
+    function (char) {
+      state = 4;
+    },
+    // 6 - SLASH
+    function (char) {
+      if (char === "/") state = 7;
+      else if (char === "*") state = 8;
+      else state = 0;
+    },
+    // 7 - LINE_COMMENT
+    function (char) {
+      if (char === "\r" || char === "\n") state = 0;
+    },
+    // 8 - MULTILINE_COMMENT
+    function (char) {
+      if (char === "*") state = 9;
+    },
+    // 9 - MULTILINE_ENDING
+    function (char) {
+      if (char === "/") state = 0;
+      else if (char !== "*") state = 8;
     }
+  ];
+  for (var i = 0, l = js.length; i < l; i++) {
+    states[state](js[i]);
   }
   return names;
 }
