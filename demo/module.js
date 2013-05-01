1a0c89cc387e3d5b245a468717874f133cb2b4bb
Tim Caswell <tim@creationix.com>
2013-05-01 01:50:36 -0500

    Fix module system

--- demo/module.js
+++ demo/module.js
@@ -1,23 +1,92 @@
 (function () {
-
 "use strict";
-window.define = define;
-window.require = makeRequire("/");
-window.require.resolve = resolve;
 
-var defs = {};
-var modules = {};
+var require = window.require = makeRequire("/");
+require.resolve = resolve;
+function makeRequire(root) {
+function require(path) {
+  return realRequire(root, path);
+}
+function requireAsync(path, callback) {
+  realRequireAsync(root, path, function (module) {
+    callback(null, module);
+  }, callback);
+}
+require.async = requireAsync;
+return require;
+}
 
 
+////////////////////////////////////////////////////////////////////////////////
+// Get source files via XHR.
+// Protect calls to get to cache and not allow concurrent requests.
+// This I/O can be very expensive.
+var files = {};
+var missing = {};
+var pendingGet = {};
+function get(path, callback, errback) {
+  if (path in files) return callback(path, files[path]);
+  if (path in missing) return errback(missing[path]);
+  if (path in pendingGet) return pendingGet[path].push([callback, errback]);
+  pendingGet[path] = [[callback, errback]];
+  realGet(path, function (contents) {
+    files[path] = contents;
+    flush();
+  }, function (err) {
+    missing[path] = err;
+    flush();
+  });
+  function flush() {
+    var pending = pendingGet[path];
+    delete pendingGet[path];
+    pending.forEach(function (pair) {
+      get(path, pair[0], pair[1]);
+    });
+  }
+}
+function realGet(path, callback, errback) {
+  var request = new XMLHttpRequest();
+  request.onload = function () {
+    callback(this.responseText);
+  };
+  request.onerror = function () {
+    errback(this);
+  };
+  request.open("GET", path, true);
+  request.setRequestHeader("Pragma", "no-cache");
+  request.send();
+}
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Use a temporary filesystem to write the wrapped js files.
+// The writeFile function outputs the url the file that a script tag can use.
 var fs;
-(window.requestFileSystem || window.webkitRequestFileSystem)(window.TEMPORARY, null, function (fileSystem) {
+var requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
+requestFileSystem(window.TEMPORARY, null, function (fileSystem) {
   fs = fileSystem;
   window.modulesReady();
 }, function (fileError) {
   throw new Error("Unable to create temporary fs for module loader: " + fileError);
 });
+function writeFile(path, contents, callback, errback) {
+  fs.root.getFile(path, {create: true}, function (fileEntry) {
+    fileEntry.createWriter(function (fileWriter) {
+      fileWriter.onwriteend = function () {
+        callback(fileEntry.toURL());
+      };
+      fileWriter.onerror = errback;
+      fileWriter.write(new Blob([contents], {type: 'text/plain'}));
+    }, errback);
+  }, errback);
+}
+////////////////////////////////////////////////////////////////////////////////
 
 
+////////////////////////////////////////////////////////////////////////////////
+// Resolve relative paths to absolute paths.
+// Also implements module lookup logic and automatic file extensions.
 // Root is always an absolute path starting and ending with a `/`
 // path is the actual string used in require.
 // If path starts with a `/`, then it's an absolute require.
@@ -26,14 +95,21 @@ var fs;
 // We also want to support optional extensions, loading json files, and
 // parsing package.json looking for main.
 // callback(realPath, contents) or errback(err) are the output.
-function realResolve(root, path, callback, errback) {
-  if (path[0] === "/") return find(path, callback, errback);
-  if (path[0] === ".") return find(realPath(root + path), callback, errback);
+var mappings = {};
+require.mappings = mappings;
+require.resolve = resolve;
+function resolve(root, path, callback, errback) {
+  function success(fullPath, contents) {
+    mappings[root + ":" + path] = fullPath;
+    callback(fullPath, contents);
+  }
+  if (path[0] === "/") return find(path, success, errback);
+  if (path[0] === ".") return find(realPath(root + path), success, errback);
   var base = root;
   cycle();
   function cycle() {
     base = base.match(/^(.*\/)[^\/]*$/)[1];
-    find(base + "modules/" + path, callback, function () {
+    find(base + "modules/" + path, success, function () {
       if (base.length > 1) {
         base = base.substr(0, base.length - 1);
         cycle();
@@ -44,37 +120,6 @@ function realResolve(root, path, callback, errback) {
     });
   }
 }
-
-// A concurrency-safe version of resolve;
-var mappings = {};
-require.defs = defs;
-require.modules = modules;
-require.mappings = mappings;
-var resolvePending = {};
-function resolve(root, path, callback, errback) {
-  var key = root + ":" + path;
-  if (key in mappings) {
-    return callback(mappings[key][0], mappings[key][1]);
-  }
-  if (key in resolvePending) {
-    return resolvePending.push([callback, errback]);
-  }
-  resolvePending[key] = [[callback, errback]];
-  realResolve(root, path, function (realPath, contents) {
-    mappings[key] = [realPath, contents];
-    flush(null, realPath, contents);
-  }, flush);
-  function flush(err, realPath, contents) {
-    var pending = resolvePending[key];
-    delete resolvePending[key];
-    pending.forEach(function (pair) {
-      if (err) pair[1](err);
-      else pair[0](realPath, contents);
-    });
-  }
-}
-
-// unify a path
 function realPath(path) {
   var match;
   // Remove /./ entries from path
@@ -87,7 +132,6 @@ function realPath(path) {
   }
   return path;
 }
-
 function find(path, callback, errback) {
   // .js extensions are passed through as-is
   if (/\.js$/.test(path)) return get(path, callback, errback);
@@ -109,165 +153,121 @@ function find(path, callback, errback) {
     });
   });
 }
+////////////////////////////////////////////////////////////////////////////////
 
-function makeRequire(root) {
-function require(path) {
-  var key = root + ":" + path;
-  if (!(key in mappings)) {
-    throw new Error("Can't load module sychronously: " + key);
+
+////////////////////////////////////////////////////////////////////////////////
+// Load loads a module and all it's dependencies, wraps them in AMD and inserts
+// them as script tags.
+function load(root, path, callback, errback) {
+  resolve(root, path, function (realPath, contents) {
+    process(realPath, contents, callback, errback);
+  }, errback);
+}
+var defs = {};
+require.defs = defs;
+var defCallbacks = {};
+window.define = define;
+function define(path, deps, fn) {
+  var def = { path: path, deps: deps, fn: fn };
+  defs[path] = def;
+  var callback = defCallbacks[path];
+  delete defCallbacks[path];
+  callback(def);
+}
+var processPending = {};
+function process(path, contents, callback, errback) {
+  if (path in defs) return callback(defs[path]);
+  if (path in processPending) {
+    return processPending[path].push([callback, errback]);
   }
-  return realRequire(mappings[key]);
-}
-function requireAsync(path, callback) {
-  resolve(root, path, function (path) {
-    realRequireAsync(path, callback);
-  }, callback);
-}
-require.async = requireAsync;
-return require;
-}
-
-// basic xhr get
-function get(path, callback, errback) {
-  console.log("GET", path);
-  var request = new XMLHttpRequest();
-  request.onload = function () {
-    callback(path, this.responseText);
-  };
-  request.onerror = function () {
-    errback(this);
-  };
-  request.open("GET", path, true);
-  request.setRequestHeader("Pragma", "no-cache");
-  request.send();
-}
-
-function writeFile(path, contents, callback, errback) {
-  fs.root.getFile(path, {create: true}, function (fileEntry) {
-    fileEntry.createWriter(function (fileWriter) {
-      fileWriter.onwriteend = function () {
-        callback(fileEntry);
-      };
-      fileWriter.onerror = errback;
-      fileWriter.write(new Blob([contents], {type: 'text/plain'}));
-    }, errback);
-  }, errback);        
-}
-
-function process(path, js) {
-  console.log("PROCESS", {path:path, js:js});
-  var root = path.match(/^(.*\/)[^\/]*$/)[1];
-  // TODO: replace with a more robust matcher
-  var matches = js.match(/require\((['"])[^)]*\1\)/g);
-  if (!matches) return;
-  for (var i = 0, l = matches.length; i < l; i++) {
-    var match = matches[i].match(/require\((['"])([^)]*)\1\)/)[2];
-    resolve(root, match, function (realPath, contents) {
-      load(realPath, function (realPath, url) {
-      });
+  processPending[path] = [[callback, errback]];
+  realProcess(path, contents, function (def) {
+    defs[path] = def;
+    flush(null, def);
+  }, flush);
+  function flush(err, def) {
+    var pending = processPending[path];
+    delete processPending[path];
+    pending.forEach(function (pair) {
+      if (err) pair[1](err);
+      else pair[0](def);
     });
   }
-  // TODO: look for dependencies in js and load them too.
 }
+function realProcess(path, contents, callback, errback) {
+  // Scan for dependencies
+  var root = path.match(/^(.*\/)[^\/]*$/)[1];
+  var matches = contents.match(/require\((['"])[^)]*\1\)/g);
+  var deps = [];
+  if (!matches) return save();
+  // If there are dependencies, load them first.
+  var left = matches.length;
+  for (var i = 0, l = left; i < l; i++) {
+    var match = matches[i].match(/require\((['"])([^)]*)\1\)/)[2];
+    resolve(root, match, onResolve, errback);
+  }
+  function onResolve(realPath, contents) {
+    deps.push(realPath);
+    process(realPath, contents, check, errback);
+  }
+  function check(dep) {
+    deps.push(dep);
+    if (--left) return;
+    deps.sort();
+    save();
+  }
 
-// Load a script from disk and place in the temporary filesystem.
-function realLoad(path, callback, errback) {
-  
-  get(path, function (path, js) {
-    process(path, js);
-    var wrappedjs =
-      'window.define(' + JSON.stringify(path) + ', function (module, exports, require, __dirname, __filename) {\n' +
-      js + '\n});';
-    // Write the wrapped file to the temporary filesystem.
-    writeFile(path.substr(1).replace(/\//g, "_"), wrappedjs, function (fileEntry) {
-      var url = fileEntry.toURL();
+  function save() {
+    // Wrap and save the file
+    var wrappedjs = 'window.define(' + JSON.stringify(path) + ', ' + JSON.stringify(deps) +
+      ', function (module, exports, require, __dirname, __filename) {\n' + contents + '\n});';
+    writeFile(path.substr(1).replace(/\//g, "_"), wrappedjs, function (url) {
+      defCallbacks[path] = callback;
       var script = document.createElement('script');
       script.type = 'text/javascript';
       script.src = url;
-      document.head.appendChild(script);    
-      callback(url);
+      document.head.appendChild(script);
     }, errback);
+  }
+}
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////
+var modules = {};
+require.modules = modules;
+function realRequire(root, path) {
+  var realPath = mappings[root + ":" + path];
+  if (!realPath) throw new Error("Can't require sync yet: " + path + " in " + root);
+  var def = defs[realPath];
+  if (!def) throw new Error("Missing definition for: " + realPath);
+  return start(def);
+}
+function realRequireAsync(root, path, callback, errback) {
+  load(root, path, function (def) {
+    var module;
+    try {
+      module = start(def);
+    }
+    catch (err) {
+      return errback(err);
+    }
+    callback(module);
   }, errback);
 }
 
-// A concurrency safe version of load.
-var urls = {};
-var loadPending = {};
-function load(path, callback, errback) {
-  // Check for cached values and short-circuit.
-  if (path in urls) {
-    return callback(null, urls[path]);
-  }
-  // Check for other concurrent loads and piggyback.
-  if (path in loadPending) {
-    return loadPending[path].push([callback, errback]);
-  }
-  loadPending[path] = [[callback, errback]];
-  realLoad(path, function (url) {
-    urls[path] = url;
-    flush(null, url);
-  }, flush);
-  function flush(err, url) {
-    var pending = loadPending[path];
-    delete loadPending[path];
-    pending.forEach(function (pair) {
-      if (err) pair[1](err);
-      else pair[0]();
-    });
-  }
-}
-
-function define(path, fn) {
-  defs[path] = fn;
-}
-
-function start(path) {
-  var pathname = "/" + path;
+function start(def) {
+  var pathname = def.path;
   var dirname = pathname.match(/^(.*\/)[^\/]*$/)[1];
   var require = makeRequire(dirname);
   var exports = {};
   var module = {exports: exports};
-  var def = defs[path];
-  delete defs[path];
-  modules[path] = exports;
-  def(module, exports, require, dirname, pathname);
-  modules[path] = module.exports;
-  return module.exports;  
-}
-
-// This is an absolute path, the require within modules is relative;
-function realRequire(path) {
-  if (path in defs) {
-    try {
-      start(path);
-    }
-    catch (err) {
-      delete defs[path];
-      throw err;
-    }
-  }
-  if (path in modules) {
-    return modules[path];
-  }
-  throw new Error("Module not found using sync: " + path);
-}
-
-function realRequireAsync(path, callback) {
-  // already loaded modules short-circuit
-  if (path in defs) {
-    try {
-      start(path);
-    }
-    catch (err) {
-      delete defs[path];
-      return callback(err);
-    }
-  }
-  if (path in modules) {
-    return callback(null, modules[path]);
-  }
-  load(path, function (url) {
-  }, callback);
+  delete defs[pathname];
+  modules[pathname] = exports;
+  def.fn(module, exports, require, dirname, pathname);
+  modules[pathname] = module.exports;
+  return module.exports;
 }
 
 }());
