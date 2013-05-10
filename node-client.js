f5881f1b12170b5b3ac421ade39ce2c49a32dcd5
Tim Caswell <tim@creationix.com>
2013-05-09 23:01:48 -0500

    Update sample git client

--- node-client.js
+++ node-client.js
@@ -12,29 +12,55 @@ function lsRemote(host, path) {
     if (err) throw err;
 
     console.log("Connected to %s, sending git-upload-pack request", host);
-    helpers.chain()
-      .addPush(app)
-      .pushWrap(pktLine.deframer, pktLine.framer)
-      .run(client.source, client.sink);
+    helpers.run([
+      client.source,
+      pktLine.deframer,
+      app,
+      pktLine.framer,
+      client.sink
+    ]);
 
   });
 
+  app.is = "min-stream-push-filter";
   function app(emit) {
     var state = "ref-discovery";
+    var refs = {};
+    var caps;
     var states = {
       "ref-discovery": function (message) {
         if (message === null) {
-          console.log("TODO: handle next state");
+          console.log({refs:refs,caps:caps});
+          var clientCaps = [
+            // "multi_ack_detailed",
+            // "side-band-64k",
+            // "thin-pack",
+            // "ofs-delta",
+            "agent=js-git/0.0.0"
+            // "agent=git/1.8.1.2"
+          ];
+          emit(null, pktLine.encode(["want", refs.HEAD].concat(clientCaps)));
+          // emit(null, pktLine.encode(["want", refs["refs/heads/master"]]));
+          emit(null, null);
+          emit(null, pktLine.encode(["done"]));
+          state = "pack";
           return;
         }
         message = pktLine.decode(message);
-        console.log(message);
+        if (message.caps) {
+          caps = message.caps;
+          delete message.caps;
+        }
+        refs[message[1]] = message[0];
+      },
+      "pack": function (message) {
+
       }
     };
     emit(null, pktLine.encode(["git-upload-pack", path], {host: host}, true));
     return function (err, item) {
       if (item === undefined) return emit(err);
-      console.log(state, toString(item));
+      console.log(state, item);
       states[state](item);
     };
   }
