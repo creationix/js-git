ca7843cf56c91d495c66623dd05338c7d06f62cb
Tim Caswell <tim@creationix.com>
2013-08-05 14:52:31 -0500

    Finish ls-remote example

--- examples/ls-remote.js
+++ examples/ls-remote.js
@@ -3,7 +3,7 @@ var url = process.argv[2] || "git://github.com/creationix/conquest.git";
 // Load a node powered lsRemote function
 var lsRemote = require('../lib/ls-remote.js')(require('./node'));
 
-lsRemote(url, function (err, refs) {
+lsRemote(url, function (err, refs, caps) {
   if (err) throw err;
   console.log(refs);
 });
