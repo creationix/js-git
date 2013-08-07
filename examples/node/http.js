d580014d05a657e424affe311eed20d429ee7de3
Tim Caswell <tim@creationix.com>
2013-08-07 13:35:05 -0500

    Make tracing more verbose

--- examples/node/http.js
+++ examples/node/http.js
@@ -6,14 +6,19 @@ module.exports = {
 
 function request(opts, callback) {
   var base = opts.tls ? require('https') : require('http');
-  if (opts.trace) opts.trace("request", null, toLine(opts));
+  if (opts.trace) opts.trace("request", null, {
+    method: opts.method,
+    host: opts.hostname,
+    port: opts.port,
+    path: opts.path,
+    headers: opts.headers
+  });
   var req = base.request(opts, function (res) {
-    if (opts.trace) opts.trace("response", null, [res.statusCode, res.headers]);
+    if (opts.trace) opts.trace("response", null, {
+      code: res.statusCode,
+      headers: res.headers
+    });
     callback(null, res.statusCode, res.headers, wrapStream(res));
   });
   req.end();
-}
-
-function toLine(opts) {
-  return opts.method + " " + opts.hostname + ":" + opts.port + " " + opts.path;
 }
