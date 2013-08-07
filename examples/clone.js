4d71f30364611c66c84b556ffcd6216ee4cf3328
Tim Caswell <tim@creationix.com>
2013-08-06 19:53:50 -0500

    Start to implement clone

--- examples/clone.js
+++ examples/clone.js
@@ -1,22 +1,17 @@
+var platform = require('./node');
+var fsDb = require('../lib/fs-db.js')(platform);
+var fetch = require('../lib/fetch.js')(platform);
 
 var url = process.argv[2] || "git://github.com/creationix/conquest.git";
 var path = url.split("/");
 path = path[path.length - 1];
 
-var platform = require('./node');
-// And create a db instance as a local bare repo
-var db = require('../lib/fs-db.js')(platform)(path, true);
-// And wrap in a repo API
-var repo = require('../lib/repo.js')(db);
-// Get the pull logic
-var pull = require('../lib/pull.js');
-
+var repo = require('../lib/repo.js')(fsDb(path, true));
 
 repo.init(function (err) {
   if (err) throw err;
-  repo.lsRemote(url, function (err, refs) {
+  fetch(repo, url, function (err, report) {
     if (err) throw err;
-    console.log("refs", refs);
+    console.log("report", report);
   });
 });
-
