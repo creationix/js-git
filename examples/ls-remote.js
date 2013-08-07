a2f01a9d6472d8dcf831229861c15e3d72398367
Tim Caswell <tim@creationix.com>
2013-08-06 22:03:04 -0500

    Clean up signatures a bit

--- examples/ls-remote.js
+++ examples/ls-remote.js
@@ -1,8 +1,6 @@
-
-var url = process.argv[2] || "git://github.com/creationix/conquest.git";
-// Load a node powered lsRemote function
 var lsRemote = require('../lib/network.js')(require('./node')).lsRemote;
 
+var url = process.argv[2] || "git://github.com/creationix/conquest.git";
 lsRemote(url, function (err, refs, caps) {
   if (err) throw err;
   Object.keys(refs).forEach(function (ref) {
