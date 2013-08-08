4ac8e01f2266b033dd1e4a483fdcff4e894891b1
Tim Caswell <tim@creationix.com>
2013-08-08 17:14:21 -0500

    Start to implement http clone

--- protocols/smart-http.js
+++ protocols/smart-http.js
@@ -5,6 +5,9 @@ var pushToPull = require('push-to-pull');
 var deframer = pushToPull(require('../lib/pkt-line.js').deframer);
 var trace = platform.require('trace');
 var sharedDiscover = require('./discover.js');
+var each = require('../helpers/each.js');
+var pktLine = require('../lib/pkt-line.js');
+var bops = require('bops');
 
 // opts.hostname - host to connect to (github.com)
 // opts.pathname - path to repo (/creationix/conquest.git)
@@ -18,30 +21,61 @@ module.exports = function (opts) {
 
   return {
     discover: discover,
-    negotiate: negotiate,
+    fetch: fetch,
     close: close
   };
 
-  // Send initial git-upload-pack request
-  // outputs refs and caps
-  function discover(callback) {
-    if (!callback) return discover.bind(this);
-      var headers = {
+  function addDefaults(extras) {
+    var headers = {
       "User-Agent": agent,
       "Host": opts.hostname,
-      "Accept": "*/*",
-      "Accept-Encoding": "gzip",
-      "Pragma": "no-cache"
     };
+    for (var key in extras) {
+      headers[key] = extras[key];
+    }
+    return headers;
+  }
 
+  function get(path, headers, callback) {
     http.request({
       method: "GET",
       hostname: opts.hostname,
       tls: opts.tls,
       port: opts.port,
-      path: opts.pathname + "/info/refs?service=git-upload-pack",
       auth: opts.auth,
-      headers: headers
+      path: opts.pathname + path,
+      headers: addDefaults(headers)
+    }, callback);
+  }
+
+  function post(path, headers, body, callback) {
+    headers = addDefaults(headers);
+    if (typeof body === "string") {
+      body = bops.from(body);
+    }
+    if (bops.is(body)) {
+      headers["Content-Length"] = body.length
+    }
+    http.request({
+      method: "POST",
+      hostname: opts.hostname,
+      tls: opts.tls,
+      port: opts.port,
+      auth: opts.auth,
+      path: path,
+      headers: headers,
+      body: body
+    }, callback);
+  }
+
+  // Send initial git-upload-pack request
+  // outputs refs and caps
+  function discover(callback) {
+    if (!callback) return discover.bind(this);
+    get("/info/refs?service=git-upload-pack", {
+      "Accept": "*/*",
+      "Accept-Encoding": "gzip",
+      "Pragma": "no-cache"
     }, function (err, code, headers, body) {
       if (err) return callback(err);
       if (code !== 200) return callback(new Error("Unexpected status code " + code));
@@ -68,9 +102,98 @@ module.exports = function (opts) {
     });
   }
 
-  function negotiate(callback) {
-    if (!callback) return negotiate.bind(this);
-    throw new Error("TODO: Implement smart-http negotiate");
+  function fetch(opts, callback) {
+    var want = opts.want,
+        have = opts.have,
+        onProgress = opts.onProgress,
+        onError = opts.onError;
+
+    if (!callback) return fetch.bind(this, opts);
+    discover(function (err, refs, serverCaps) {
+      if (err) return callback(err);
+
+      var caps = [];
+      if (serverCaps["ofs-delta"]) caps.push("ofs-delta");
+      if (serverCaps["thin-pack"]) caps.push("thin-pack");
+      if (opts.includeTag && serverCaps["include-tag"]) caps.push("include-tag");
+      if ((opts.onProgress || opts.onError) &&
+          (serverCaps["side-band-64k"] || serverCaps["side-band"])) {
+        caps.push(serverCaps["side-band-64k"] ? "side-band-64k" : "side-band");
+        if (!opts.onProgress && serverCaps["no-progress"]) {
+          caps.push("no-progress");
+        }
+      }
+      if (serverCaps.agent) caps.push("agent=" + agent);
+
+      if (want) throw new Error("TODO: Implement dynamic wants");
+      if (have) throw new Error("TODO: Implement dynamic have");
+
+      var wants = [];
+      each(refs, function (name, hash) {
+        if (name === "HEAD" || name.indexOf('^') > 0) return;
+        wants.push("want " + hash);
+      });
+
+      wants[0] += " " + caps.join(" ");
+      wants.push(null, "done\n");
+
+      var body = [];
+      wants.forEach(function (line) {
+        if (line === null) {
+          return body.push(bops.from("0000"));
+        }
+        line = bops.from(line);
+        body.push(pktLine.frameHead(line), line);
+      });
+      body = bops.join(body);
+
+      post("/git-upload-pack", {
+        "Content-Type": "application/x-git-upload-pack-request",
+        "Accept": "application/x-git-upload-pack-result",
+      }, body, function (err, code, headers, body) {
+        if (err) return callback(err);
+        console.log({code:code,headers:headers,body:body});
+      })
+
+//   var packStream = writable(abort);
+
+//   read(function (err, nak) {
+//     if (err) return callback(err);
+//     if (nak.trim() !== "NAK") {
+//       return callback(Error("Expected NAK"));
+//     }
+//     callback(null, {
+//       read: packStream.read,
+//       abort: packStream.abort,
+//       refs: refs
+//     });
+//     read(onItem);
+//   });
+
+//   function onItem(err, item) {
+//     if (err) return packStream.error(err);
+//     if (item) {
+//       if (item.progress) {
+//         if (opts.onProgress) opts.onProgress("remote: " + item.progress);
+//       }
+//       else if (item.error) {
+//         if (opts.onError) opts.onError(item.error);
+//       }
+//       else {
+//         packStream(item);
+//       }
+//     }
+//     if (item === undefined) {
+//       packStream(undefined);
+//     }
+//     else read(onItem);
+//   }
+
+// }
+
+
+
+    });
   }
 
   function close(callback) {
