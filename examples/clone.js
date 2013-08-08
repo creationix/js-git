d5b7824a9c6d20ddabf2d0be0eb66e15a3a2a6bb
Tim Caswell <tim@creationix.com>
2013-08-07 23:01:56 -0500

    Convert broken clone sample to new platform style

--- examples/clone.js
+++ examples/clone.js
@@ -1,7 +1,11 @@
+// Bootstrap the platform to run on node.js
+require('../lib/platform.js')(require('./node'));
+
+// Load the libraries
+var fsDb = require('../lib/fs-db.js');
+var wrap = require('../lib/repo.js');
 var each = require('../helpers/each.js');
-var platform = require('./node');
-var proto = require('../protocols/auto.js')(platform);
-var fsDb = require('../lib/fs-db.js')(platform);
+var autoProto = require('../protocols/auto.js');
 var urlParse = require('url').parse;
 
 var url = process.argv[2] || "git://github.com/creationix/conquest.git";
@@ -11,8 +15,9 @@ if (!opts.protocol) {
 }
 if (process.env.TRACE) opts.trace = require('./trace.js');
 var path = opts.pathname.match(/[^\/]*$/)[0];
-var connection = proto(opts);
-var repo = require('../lib/repo.js')(fsDb(path, true));
+
+var connection = autoProto(opts);
+var repo = wrap(fsDb(path, true));
 
 connection.discover(function (err, result) {
   if (err) throw err;
