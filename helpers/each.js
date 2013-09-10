da81d1156ead189cb5f10e655988bd40a0f7bdf4
Tim Caswell <tim@creationix.com>
2013-09-10 14:02:20 -0500

    Clean out stuff

--- helpers/each.js
+++
@@ -1,8 +0,0 @@
-module.exports = each;
-function each(obj, fn) {
-  var keys = Object.keys(obj);
-  for (var i = 0, l = keys.length; i < l; i++) {
-    var key = keys[i];
-    fn(key, obj[key]);
-  }
-}
