8c5ad9fb00b78daa54c7b2c50fdb3f56bd1114b2
Tim Caswell <tim@creationix.com>
2013-09-25 10:24:45 -0500

    Finish implementing new walker API

--- js-git.js
+++ js-git.js
@@ -181,14 +181,40 @@ function newRepo(db, workDir) {
 
   function treeWalk(hashish, callback) {
     if (!callback) return treeWalk.bind(this, hashish);
-    // return load(hashish, onLoad);
-    // function onLoad(err, item, hash) {
-    //   if (err) return callback(err);
-    //   if (item.type === "commit") return load(item.body.tree, onLoad);
-    //   item.hash = hash;
-    //   item.path = "/";
-    //   return callback(null, walk(item, treeScan, treeCompare, {}));
-    // }
+    return load(hashish, onLoad);
+    function onLoad(err, item, hash) {
+      if (err) return callback(err);
+      if (item.type === "commit") return load(item.body.tree, onLoad);
+      item.hash = hash;
+      item.path = "/";
+      return callback(null, walk(item, treeScan, treeLoadKey, treeCompare));
+    }
+  }
+
+  function treeScan(object) {
+    if (object.type === "blob") return [];
+    assertType(object, "tree");
+    return object.body.filter(function (entry) {
+      return entry.mode !== 0160000;
+    }).map(function (entry) {
+      var path = object.path + entry.name;
+      if (entry.mode === 040000) path += "/";
+      entry.path = path;
+      return entry;
+    });
+  }
+
+  function treeLoadKey(entry, callback) {
+    return load(entry.hash, function (err, object) {
+      if (err) return callback(err);
+      entry.type = object.type;
+      entry.body = object.body;
+      return callback(null, entry);
+    });
+  }
+
+  function treeCompare(first, second) {
+    return first.path < second.path;
   }
 
   function walk(seed, scan, loadKey, compare) {
