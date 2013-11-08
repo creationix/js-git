379508704a7c4bd9b8e8178003b1b7e30ba1c5b5
Tim Caswell <tim@creationix.com>
2013-11-08 10:12:36 -0600

    Break out object store methods to a mixin module

--- js-git.js
+++ js-git.js
@@ -1,10 +1,5 @@
-var parseAscii = require('./lib/parseascii.js');
-var encoders = require('./lib/encoders.js');
-var decoders = require('./lib/decoders.js');
-var frame = require('./lib/frame.js');
-var deframe = require('./lib/deframe.js');
-var sha1 = require('./lib/sha1.js');
 var trace = require('./lib/trace.js');
+
 var treeWalk = require('./lib/tree-walk.js');
 var logWalk = require('./lib/log-walk.js');
 var fetch = require('./lib/fetch.js');
@@ -18,16 +13,16 @@ function newRepo(db) {
 
   var repo = {};
 
-  repo.db = db;
-
+  // Auto trace the db if tracing is turned on.
   if (trace) db = require('./lib/tracedb.js')(db);
 
+  // Add the db interface (used by objects, refs, and unpack mixins)
+  repo.db = db;
+
+  // Add in object store interfaces
+  require('./mixins/objects.js')(repo);
+
   // Git Objects
-  repo.load = load;       // (hash-ish) -> object
-  repo.save = save;       // (object) -> hash
-  repo.loadAs = loadAs;   // (type, hash-ish) -> value
-  repo.saveAs = saveAs;   // (type, value) -> hash
-  repo.remove = remove;   // (hash-ish)
   repo.unpack = unpack;   // (opts, packStream)
 
   // Convenience Readers
@@ -35,14 +30,14 @@ function newRepo(db) {
   repo.treeWalk = treeWalk; // (hash-ish) => stream<object>
 
   // Refs
-  repo.resolveHashish = resolveHashish; // (hash-ish) -> hash
-  repo.updateHead = updateHead;         // (hash)
-  repo.getHead = getHead;               // () -> ref
-  repo.setHead = setHead;               // (ref)
-  repo.readRef = readRef;               // (ref) -> hash
-  repo.createRef = createRef;           // (ref, hash)
-  repo.deleteRef = deleteRef;           // (ref)
-  repo.listRefs = listRefs;             // (prefix) -> refs
+  repo.resolve = resolve;       // (hash-ish) -> hash
+  repo.updateHead = updateHead; // (hash)
+  repo.getHead = getHead;       // () -> ref
+  repo.setHead = setHead;       // (ref)
+  repo.readRef = readRef;       // (ref) -> hash
+  repo.createRef = createRef;   // (ref, hash)
+  repo.deleteRef = deleteRef;   // (ref)
+  repo.listRefs = listRefs;     // (prefix) -> refs
 
   // Network Protocols
   repo.fetch = fetch;
@@ -50,94 +45,9 @@ function newRepo(db) {
 
   return repo;
 
-  function load(hashish, callback) {
-    if (!callback) return load.bind(this, hashish);
-    var hash;
-    return resolveHashish(hashish, onHash);
 
-    function onHash(err, result) {
-      if (result === undefined) return callback(err);
-      hash = result;
-      return db.get(hash, onBuffer);
-    }
-
-    function onBuffer(err, buffer) {
-      if (err) return callback(err);
-      var type, object;
-      try {
-        if (sha1(buffer) !== hash) {
-          throw new Error("Hash checksum failed for " + hash);
-        }
-        var pair = deframe(buffer);
-        type = pair[0];
-        buffer = pair[1];
-        object = {
-          type: type,
-          body: decoders[type](buffer)
-        };
-      } catch (err) {
-        if (err) return callback(err);
-      }
-      return callback(null, object, hash);
-    }
-  }
-
-  function save(object, callback) {
-    if (!callback) return save.bind(this, object);
-    var buffer, hash;
-    try {
-      buffer = encoders[object.type](object.body);
-      buffer = frame(object.type, buffer);
-      hash = sha1(buffer);
-    }
-    catch (err) {
-      return callback(err);
-    }
-    return db.set(hash, buffer, onSave);
-
-    function onSave(err) {
-      if (err) return callback(err);
-      return callback(null, hash);
-    }
-  }
-
-  function loadAs(type, hashish, callback) {
-    if (!callback) return loadAs.bind(this, type, hashish);
-    return load(hashish, onObject);
-
-    function onObject(err, object, hash) {
-      if (object === undefined) return callback(err);
-      if (type === "text") {
-        type = "blob";
-        object.body = parseAscii(object.body, 0, object.body.length);
-      }
-      if (object.type !== type) {
-        return new Error("Expected " + type + ", but found " + object.type);
-      }
-      return callback(null, object.body, hash);
-    }
-  }
-
-  function saveAs(type, body, callback) {
-    if (!callback) return saveAs.bind(this, type, body);
-    if (type === "text") type = "blob";
-    return save({ type: type, body: body }, callback);
-  }
-
-  function remove(hashish, callback) {
-    if (!callback) return remove.bind(this, hashish);
-    var hash;
-    return resolveHashish(hashish, onHash);
-
-    function onHash(err, result) {
-      if (err) return callback(err);
-      hash = result;
-      return db.del(hash, callback);
-    }
-  }
-
-  function resolveHashish(hashish, callback) {
-    if (!callback) return resolveHashish.bind(this, hashish);
+  function resolve(hashish, callback) {
+    if (!callback) return resolve.bind(this, hashish);
     hashish = hashish.trim();
     if ((/^[0-9a-f]{40}$/i).test(hashish)) {
       return callback(null, hashish.toLowerCase());
@@ -151,13 +61,13 @@ function newRepo(db) {
     function onBranch(err, ref) {
       if (err) return callback(err);
       if (!ref) return callback();
-      return resolveHashish(ref, callback);
+      return resolve(ref, callback);
     }
 
     function checkBranch(err, hash) {
       if (err && err.code !== "ENOENT") return callback(err);
       if (hash) {
-        return resolveHashish(hash, callback);
+        return resolve(hash, callback);
       }
       return db.get("refs/heads/" + hashish, checkTag);
     }
@@ -165,7 +75,7 @@ function newRepo(db) {
     function checkTag(err, hash) {
       if (err && err.code !== "ENOENT") return callback(err);
       if (hash) {
-        return resolveHashish(hash, callback);
+        return resolve(hash, callback);
       }
       return db.get("refs/tags/" + hashish, final);
     }
@@ -173,7 +83,7 @@ function newRepo(db) {
     function final(err, hash) {
       if (err) return callback(err);
       if (hash) {
-        return resolveHashish(hash, callback);
+        return resolve(hash, callback);
       }
       err = new Error("ENOENT: Cannot find " + hashish);
       err.code = "ENOENT";
---
+++ mixins/objects.js
@@ -0,0 +1,114 @@
+var sha1 = require('../lib/sha1.js');
+var frame = require('../lib/frame.js');
+var deframe = require('../lib/deframe.js');
+var encoders = require('../lib/encoders.js');
+var decoders = require('../lib/decoders.js');
+var parseAscii = require('../lib/parseascii.js');
+
+// Add "objects" capabilities to a repo using db as storage.
+module.exports = function (repo) {
+
+  // Add Object store capability to the system
+  repo.load = load;     // (hash-ish) -> object
+  repo.save = save;     // (object) -> hash
+  repo.loadAs = loadAs; // (type, hash-ish) -> value
+  repo.saveAs = saveAs; // (type, value) -> hash
+  repo.remove = remove; // (hash)
+
+  // This is a fallback resolve in case there is no refs system installed.
+  if (!repo.resolve) repo.resolve = function (hash, callback) {
+    if (isHash(hash)) return callback(null, hash);
+    return callback(new Error("This repo only supports direct hashes"));
+  };
+
+};
+
+function load(hashish, callback) {
+  if (!callback) return load.bind(this, hashish);
+  var hash;
+  var repo = this;
+  var db = repo.db;
+  return repo.resolve(hashish, onHash);
+
+  function onHash(err, result) {
+    if (result === undefined) return callback(err);
+    hash = result;
+    return db.get(hash, onBuffer);
+  }
+
+  function onBuffer(err, buffer) {
+    if (err) return callback(err);
+    var type, object;
+    try {
+      if (sha1(buffer) !== hash) {
+        throw new Error("Hash checksum failed for " + hash);
+      }
+      var pair = deframe(buffer);
+      type = pair[0];
+      buffer = pair[1];
+      object = {
+        type: type,
+        body: decoders[type](buffer)
+      };
+    } catch (err) {
+      if (err) return callback(err);
+    }
+    return callback(null, object, hash);
+  }
+}
+
+function save(object, callback) {
+  if (!callback) return save.bind(this, object);
+  var buffer, hash;
+  var repo = this;
+  var db = repo.db;
+  try {
+    buffer = encoders[object.type](object.body);
+    buffer = frame(object.type, buffer);
+    hash = sha1(buffer);
+  }
+  catch (err) {
+    return callback(err);
+  }
+  return db.set(hash, buffer, onSave);
+
+  function onSave(err) {
+    if (err) return callback(err);
+    return callback(null, hash);
+  }
+}
+
+function remove(hash, callback) {
+  if (!callback) return remove.bind(this, hash);
+  if (!isHash(hash)) return callback(new Error("Invalid hash: " + hash));
+  var repo = this;
+  var db = repo.db;
+  return db.del(hash, callback);
+}
+
+function loadAs(type, hashish, callback) {
+  if (!callback) return loadAs.bind(this, type, hashish);
+  return this.load(hashish, onObject);
+
+  function onObject(err, object, hash) {
+    if (object === undefined) return callback(err);
+    if (type === "text") {
+      type = "blob";
+      object.body = parseAscii(object.body, 0, object.body.length);
+    }
+    if (object.type !== type) {
+      return new Error("Expected " + type + ", but found " + object.type);
+    }
+    return callback(null, object.body, hash);
+  }
+}
+
+function saveAs(type, body, callback) {
+  if (!callback) return saveAs.bind(this, type, body);
+  if (type === "text") type = "blob";
+  return this.save({ type: type, body: body }, callback);
+}
+
+function isHash(hash) {
+  return (/^[0-9a-f]{40}$/).test(hash);
+}
