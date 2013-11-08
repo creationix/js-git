e32bfa58d9c905fb5b73d3d32c8bd3b8c894dc46
Tim Caswell <tim@creationix.com>
2013-11-08 10:26:42 -0600

    Move walkers into mixin

--- js-git.js
+++ js-git.js
@@ -1,6 +1,3 @@
-
-var treeWalk = require('./lib/tree-walk.js');
-var logWalk = require('./lib/log-walk.js');
 var fetch = require('./lib/fetch.js');
 var push = require('./lib/push.js');
 var unpack = require('./lib/unpack.js');
@@ -24,13 +21,11 @@ function newRepo(db) {
   // Mix in the references interface
   require('./mixins/refs.js')(repo);
 
+  // Mix in the walk helpers
+  require('./mixins/walkers.js')(repo);
+
   // Git Objects
   repo.unpack = unpack;   // (opts, packStream)
-
-  // Convenience Readers
-  repo.logWalk = logWalk;   // (hash-ish) => stream<commit>
-  repo.treeWalk = treeWalk; // (hash-ish) => stream<object>
-
 
   // Network Protocols
   repo.fetch = fetch;
