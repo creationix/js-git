4709775dfd4d350d0fc2c1f1a606310367f94ae8
Tim Caswell <tim@creationix.com>
2014-02-08 16:52:17 -0600

    Import code from tedit-app

---
+++ mixins/read-combiner.js
@@ -0,0 +1,32 @@
+/*global define*/
+define('js-git/mixins/read-combiner', function () {
+  "use strict";
+
+  // This replaces loadAs with a version that batches concurrent requests for
+  // the same hash.
+  return function (repo) {
+    var pendingReqs = {};
+
+    var loadAs = repo.loadAs;
+    repo.loadAs = newLoadAs;
+
+    function newLoadAs(type, hash, callback) {
+      if (!callback) return newLoadAs.bind(null, type, hash);
+      var list = pendingReqs[hash];
+      if (list) {
+        if (list.type !== type) callback(new Error("Type mismatch"));
+        else list.push(callback);
+        return;
+      }
+      list = pendingReqs[hash] = [callback];
+      list.type = type;
+      loadAs.call(repo, type, hash, function () {
+        delete pendingReqs[hash];
+        for (var i = 0, l = list.length; i < l; i++) {
+          list[i].apply(this, arguments);
+        }
+      });
+    }
+  };
+
+});
