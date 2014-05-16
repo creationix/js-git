2904518b6709b6d8b7e68112ac04babb9681ccca
Tim Caswell <tim@creationix.com>
2014-05-16 16:16:02 +0000

    Implement path-to-entry mixin and git-fs adapter.

---
+++ mixins/path-to-entry.js
@@ -0,0 +1,42 @@
+var cache = require('./mem-cache').cache;
+var modes = require('../lib/modes');
+
+module.exports = function (repo) {
+  repo.pathToEntry = pathToEntry;
+};
+
+function pathToEntry(rootTree, path, callback) {
+  if (!callback) return pathToEntry.bind(this, path);
+  var repo = this;
+  var mode = modes.tree;
+  var hash = rootTree;
+  var parts = path.split("/").filter(Boolean);
+  var index = 0;
+  var cached;
+  function loop() {
+    while (index < parts.length) {
+      if (mode === modes.tree) {
+        cached = cache[hash];
+        if (!cached) return repo.loadAs("tree", hash, onLoad);
+        var entry = cached[parts[index]];
+        if (!entry) return callback();
+        mode = entry.mode;
+        hash = entry.hash;
+        index++;
+        continue;
+      }
+      return callback();
+    }
+    callback({
+      mode: mode,
+      hash: hash
+    });
+  }
+
+  function onLoad(err, value) {
+    if (!value) return callback(err || new Error("Missing object: " + hash));
+    cache[hash] = value;
+    loop();
+  }
+
+}
