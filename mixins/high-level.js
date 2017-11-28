5a23cd378eda2d587d765c34e1bc7e9a28a541ab
Jasmin Besic <jbesic@capeannenterprises.com>
2017-11-28 14:17:45 +0100

    Collect files function

--- mixins/high-level.js
+++ mixins/high-level.js
@@ -23,6 +23,7 @@ function highLevel(repo, uName, uPass, hostName) {
   repo.clone = clone;
   repo.commit = commit;
   repo.push = push;
+  repo.resolveRepo = resolveRepo;
 
   function clone(callback) {
     fetch.take(function (err, refs) {
@@ -38,15 +39,136 @@ function highLevel(repo, uName, uPass, hostName) {
       fetch.take(function (err, channels) {
         repo.unpack(channels.pack, {}, function () {
           repo.updateRef('refs/heads/master', refs['refs/heads/master'], function () {
-            callback('Clonned !');
+            return callback('Clonned !');
           });
         });
       });
     });
   }
 
-  function commit() {}
+  function commit(callback) {
+    repo.readRef('refs/heads/master', function(err, refHash) {
+      repo.loadAs('commit', refHash, function(err, commit) {
+        // Changes to files that already exists
+        var changes = [
+          {
+              path: "/test/justAdded22.txt",
+              mode: modes.file,
+              content: ""
+          },
+          {
+              path: "/test/second.txt",
+              mode: modes.file,
+              content: "This is the updated content 111safa."
+          }
+        ];
+        changes['base'] = commit.tree;
 
-  function push() {}
+        repo.createTree(changes, function(err, treeHash) {
+          var commitMessage = {
+            author: {
+                name: commit.author.name,
+                email: commit.author.email
+            },
+            tree: treeHash,
+            parent: refHash,
+            message: "This is the commit message.\n"
+          }
+
+          repo.saveAs('commit', commitMessage, function(err, commitHash) {
+            repo.updateRef('refs/heads/master', commitHash, function(err, res) {
+              return callback('Commit done !');
+            });
+          });
+        });
+      });
+    });
+  }
+
+  function push(callback) {
+    repo.readRef('refs/heads/master', function(err, refHash) {
+      repo.loadAs('commit', refHash, function(err, commit) {
+        push.take(function() {
+          push.put({ oldhash: commit.parents[0], newhash: refHash, ref: 'refs/heads/master' });
+          push.put(null);
+
+          var hashes = [refHash];
+          repo.treeWalk(commit.tree, function(err, item) {
+            function collectHashes(err, object) {
+              if (object !== undefined) {
+                hashes.push(object);
+                item.read(collectHashes);
+              } else {
+                repo.pack(hashes, {}, function(err, stream) {
+                  function putHashes(err, packObject) {
+                    if (packObject !== undefined) {
+                      push.put(packObject);
+                      stream.take(putHashes);
+                    } else {
+                      push.put({flush: true});
+                      return callback('Push done !');
+                    }
+                  }
+
+                  stream.take(putHashes);
+                });
+              }
+            }
+
+            item.read(collectHashes);
+          });
+        });
+      });
+    });
+  }
+
+  function resolveRepo(callback) {
+    repo.readRef('refs/heads/master', function(err, refHash) {
+      repo.loadAs('commit', refHash, function(err, commit) {
+        if (commit === undefined) { return callback(); }
+
+        var files = [];
+        repo.treeWalk(commit.tree, function(err, item) {
+          /*
+            {
+              '/': {
+                mode: xxx,
+                hash: xzz,
+                'folder 1': {
+                  mode: xxx,
+                  hash: xzz,
+                  text.txt: {
+                    mode: xxx,
+                    hash: xzz,
+                    content: 'asasgfasgagga'
+                  }
+                }
+              }
+            }
+          */
+          function collectFiles(err, object) {
+            if (object !== undefined) {
+              var loadType = object.mode === 16384 ? 'tree' : 'text';
+              console.log(object);
+              var pathArray = object.path.split('/').filter(function(element) {
+                return element.length !== 0;
+              });
+
+              console.log(pathArray);
+              repo.loadAs(loadType, object.hash, function(err, content) {
+                //console.log(content);
+                //files.push(content);
+                item.read(collectFiles);
+              });
+            } else {
+              return callback(files);
+            }
+          }
+
+          item.read(collectFiles);
+        });
+      });
+    });
+  }
 
 }
